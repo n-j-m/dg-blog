c4af3a66a5dcb240adb5dd1cf1e564ca51e11db4
n-j-m <nick65@gmail.com>
2016-06-23 14:36:57 -0500

    Adds config for Heroku deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +126,21 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Login settings
 LOGIN_REDIRECT_URL = '/'
+
+
+DATABASES['default'] = dj_database_url.config()
+
+
+SECURE_PROXY_SSL_HANDLER = ('HTTP_X_FORWARDED_PROTO', 'https',)
+
+
+ALLOWED_HOSTS = ['*']
+
+
+DEBUG = False
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
