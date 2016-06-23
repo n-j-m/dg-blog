5d08ae421da1737095478cecd3f4c53e484222f0
n-j-m <nick65@gmail.com>
2016-06-23 13:34:40 -0500

    Adds permissions and authentication to blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'blog',
 ]
 
@@ -121,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Login settings
+LOGIN_REDIRECT_URL = '/'
